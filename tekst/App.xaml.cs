7e97f4feaa0b0bceef1da46e0f81e6554bd0318c
f <myemail@real.eu>
2024-11-18 18:39:25 +0100

    Add project files.

---
+++ tekst/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace tekst
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window();
+            window.MinimumHeight = 900;
+            window.MaximumHeight = 900;
+            window.MinimumWidth = 650;
+            window.MaximumWidth = 650;
+
+            window.Page = new AppShell();
+            return window;
+        }
+    }
+}
---
+++ tekst/MainPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿namespace tekst
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ileWyrazow(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string s = entSlowa.Text;
+
+            wyczyscTekst(s);
+            string wyrzucSpacje(string s)
+            {
+                while (s.Contains("  "))
+                {
+                    s = s.Replace("  ", " ");
+                }
+                return s;
+            }
+            if (string.IsNullOrEmpty(s) || string.IsNullOrWhiteSpace(s))
+            {
+                await DisplayAlert("Ojoj", "brak znakow", "sory memory");
+                return;
+            }
+            else
+                counter++;
+
+            string napis = s.Trim();
+
+            foreach (char a in napis)
+            {
+                if (a.ToString() == " ")
+                    counter++;
+            }
+
+            lblDymek.IsVisible = true;
+            imgDymek.IsVisible = true;
+            lblDymek.Text = counter.ToString();
+            imgDymek.Source = "dymek.png";
+
+            //  await DisplayAlert("O tyle", counter.ToString(), "kto pytal");
+
+        }
+
+        private async void policzSamogloski(object sender, EventArgs e)
+        {
+            string samogloski = "aąeęioóuy";
+            string s = entSlowa.Text;
+            int counter = 0;
+
+            if (string.IsNullOrEmpty(s) || string.IsNullOrWhiteSpace(s))
+            {
+                await DisplayAlert("Ojoj", "brak znakow", "sory memory");
+                return;
+            }
+
+            foreach (char a in s)
+            {
+                if (samogloski.Contains(a.ToString().ToLower()))
+                    counter++;
+            }
+
+            lblDymek2.IsVisible = true;
+            imgDymek2.IsVisible = true;
+            lblDymek2.Text = counter.ToString();
+            imgDymek2.Source = "dymek.png";
+            //await DisplayAlert("ile", counter.ToString(), "ok");
+        }
+
+        string wyczyscTekst(string st)
+        {
+            string wynik = "";
+            string zakazane = " .,;-()?!";
+            foreach (var c in zakazane)
+                if (st.Contains(c))
+                    st = st.Replace(c.ToString(), "");
+
+            wynik = st.Replace(" ", "");
+
+            return wynik;
+        }
+
+        private async void czyPalindrom(object sender, EventArgs e)
+        {
+            string s = entSlowa.Text;
+
+            if (string.IsNullOrEmpty(s) || string.IsNullOrWhiteSpace(s))
+            {
+                await DisplayAlert("Ojoj", "brak znakow", "sory memory");
+                return;
+            }
+
+            s = wyczyscTekst(s);
+
+            char[] characterki = s.ToCharArray();
+            Array.Reverse(characterki);
+            string s2 = new string(characterki);
+
+            lblDymek3.IsVisible = true;
+            imgDymek3.IsVisible = true;
+            imgDymek3.Source = "dymek2.png";
+            if (s.ToLower().Equals(s2.ToLower()))
+                lblDymek3.Text = "a jest";
+            else
+                lblDymek3.Text = "a nie jest";
+        }
+
+        private void resetuj(object sender, EventArgs s)
+        {
+            lblDymek.IsVisible = false;
+            lblDymek2.IsVisible = false;
+            lblDymek3.IsVisible = false;
+            imgDymek.IsVisible = false;
+            imgDymek2.IsVisible = false;
+            imgDymek3.IsVisible = false;
+        }
+    }
+
+}
